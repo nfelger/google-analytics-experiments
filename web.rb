134900bae46dfced88e5816a4dfe1f66ec5df378
Niko Felger <niko.felger@gmail.com>
2012-10-21 16:22:13 +0100

    Experiments set up

--- web.rb
+++ web.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
-get '/' do
-  erb :'index.html'
+get '/a' do
+  erb :variant, :locals => { :variant => :a }
+end
+
+get '/b' do
+  erb :variant, :locals => { :variant => :b }
+end
+
+get '/c' do
+  erb :variant, :locals => { :variant => :c }
+end
+
+get '/goal' do
+  erb :goal
 end
